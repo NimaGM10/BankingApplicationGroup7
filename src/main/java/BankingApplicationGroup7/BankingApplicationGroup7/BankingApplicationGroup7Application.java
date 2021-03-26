615ce0b3f7a67ffcede5bd1f2319078d81c9ebaa
NimaGm10 <nimaghaffari12@gmail.com>
2021-03-26 14:39:36 -0700

    Testing commit by Nima

--- src/main/java/BankingApplicationGroup7/BankingApplicationGroup7/BankingApplicationGroup7Application.java
+++ src/main/java/BankingApplicationGroup7/BankingApplicationGroup7/BankingApplicationGroup7Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingApplicationGroup7Application {
 
+	//Testing a commit by Nima
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplicationGroup7Application.class, args);
 	}
