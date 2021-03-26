7f6ff338679756580ff4c91758c97ff8bddef18f
NimaGm10 <nimaghaffari12@gmail.com>
2021-03-26 14:28:58 -0700

    Setup of files - Nima

---
+++ src/main/java/BankingApplicationGroup7/BankingApplicationGroup7/BankingApplicationGroup7Application.java
@@ -0,0 +1,13 @@
+package BankingApplicationGroup7.BankingApplicationGroup7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplicationGroup7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplicationGroup7Application.class, args);
+	}
+
+}
---
+++ src/test/java/BankingApplicationGroup7/BankingApplicationGroup7/BankingApplicationGroup7ApplicationTests.java
@@ -0,0 +1,13 @@
+package BankingApplicationGroup7.BankingApplicationGroup7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationGroup7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
